7f6476399bbb8882e236c808d9d4ed7a27c06b35
Mike <mikedark@yandex.ru>
2017-05-02 14:35:59 +0300

    данные необходимо ввести

--- lastlaba/Form1.cs
+++ lastlaba/Form1.cs
@@ -16,5 +16,32 @@ namespace lastlaba
         {
             InitializeComponent();
         }
+        static int Adaptive_adder(int[,] w, int[]x, int j,int m)
+        {
+            int y = 0;
+            for (int i = 1; i < m; i++)
+            {
+                y += w[j, i] * x[i];
+            }
+            return y + w[j, 0];
+        }
+        static double distance(double x_central, double y_central,double x_point, double y_point)
+        {
+            return Math.Sqrt(Math.Pow(Math.Abs(x_central-x_point), 2) + Math.Pow(Math.Abs(y_central-y_point),2)); ;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            int x = 100;
+            int y = 100;
+            int width = 10;
+            int height = 10;
+            g.DrawEllipse(Pens.Black, x, y, width, height);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
